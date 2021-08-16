be596afcb8fc9828b97c380a43db13736932910e
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-16 23:20:01 +0530

    Python.py

---
+++ 01/06.py
@@ -0,0 +1,13 @@
+# Create a function that finds the maximum range of a triangle's third edge, 
+# where the side lengths are all integers.
+
+print("reate a function that finds the maximum range of a triangle's third edge")
+
+def max_tri():
+    side1 = int(input("Enter the integer - side1 : "))
+
+
+max_tri()
+
+# # Program  not closed emmidately 
+input("Please Click to Enter to End the Program ")
