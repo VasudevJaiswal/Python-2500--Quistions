ad4832ca893c810d275ff641c5c807707b4dda7f
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-16 17:43:22 +0530

    Python.py

---
+++ 01/03.py
@@ -0,0 +1,11 @@
+# Convert Minutes into Seconds
+
+print("Convert Minutes into Seconds")
+
+num = int(input("Enter Minutes : "))
+con = num*60
+print("Minutes is Succesfully Coverted in Seconds  Result is -  ", str(con))
+
+
+# Program  not closed emmidately 
+input("Please Click to Enter to End the Program ")
