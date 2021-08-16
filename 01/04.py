71757d54a6c7b4b3ba7383f2119060f97da617d2
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-16 17:54:46 +0530

    Python.py

--- 01/04.py
+++ 01/04.py
@@ -1,3 +1,15 @@
 # Area of a Triangle
 
 print("Write a function that takes the base and height of a triangle and return its area.")
+
+def area():
+    base = float(int(input("Enter the Base of the Triangle : ")))
+    height = float(int(input("Enter the height of the Triangle : ")))
+    total_area = (base * height)/2
+    print(total_area)
+
+area()
+
+
+# Program  not closed emmidately 
+input("Please Click to Enter to End the Program ")
