691b949bf3d719727092b5f34072b23fc48ad97e
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-17 02:07:21 +0530

    Python.py

--- 01/12.py
+++ 01/12.py
@@ -2,3 +2,6 @@
 
 print("Given an n-sided regular polygon n, return the total sum of internal angles (in degrees).")
 
+n_sided = int(input("Enter your n-sided polygon : "))
+angles = 2(n_sided)−4×90(n_sided)
+print(angles)
---
+++ formula/Polygon_Angles.md
@@ -0,0 +1,5 @@
+In a regular polygon of n sides, all angles are equal. Therefore, 
+***
+##each interior angle = (2n−4)×90°n. 
+***
+ A quadrilateral is a polygon for which n = 4.
