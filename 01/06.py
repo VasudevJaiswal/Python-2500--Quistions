94777d46ea52fa187b541591ff733ffb2c9e7e3b
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-16 23:23:47 +0530

    Python.py

--- 01/06.py
+++ 01/06.py
@@ -5,6 +5,10 @@ print("reate a function that finds the maximum range of a triangle's third edge"
 
 def max_tri():
     side1 = int(input("Enter the integer - side1 : "))
+    side2 = int(input("Enter the integer - side2 : "))
+    maximum_triangle = (side1+side2)-1
+    print("Maximum range of a Triangle's third edge is - ",maximum_triangle)
+    return maximum_triangle
 
 
 max_tri()
