9c33e7522fa60903d6abc7ead659eb174ba3868e
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-16 17:20:05 +0530

    Python.py

---
+++ 01/02.py
@@ -0,0 +1,13 @@
+# Return the Next Number from the Integer Passed
+
+print("Create a function that takes a number as an argument, increments the number by +1 and returns the result.")
+
+def increments():
+    i = 0
+    num = int(input("Enter Your Number : "))
+    while i<num:
+        print("num")
+        i = i+1
+
+
+increments()
--- 01/2.py
+++
@@ -1,8 +0,0 @@
-def addition():
-    a = int(input("Enter Fisrt Number : "))
-		b = int(input("Enter Seconed Number : "))
-		sum = a+b
-        print("Sum of a and b is ", str(sum))
-		return sum
-
-addition()
