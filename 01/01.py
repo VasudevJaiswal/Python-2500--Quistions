0bcbe891d6292a041353e8c28a5b37bf70c7d5bb
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-16 17:05:45 +0530

    Python.py

--- 01/01.py
+++ 01/01.py
@@ -1,11 +1,16 @@
 # Return the Sum of Two Numbers
 
 
+print("Create a function that takes two numbers as arguments and return their sum.")
+def addition():
+    a = int(input("Enter the first Number : "))
+    b = int(input("Enter the Seconed Number : "))
+    sum = a+b
+    print("Sum of a and b is ", str(sum))
+    return sum
 
-def addition(a, b):
-    sum = print(a+b)
-    return
+addition()
 
-addition(2,3)
+
 
 
