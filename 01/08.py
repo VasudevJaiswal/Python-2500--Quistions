d34f55113da6043897d5362673d2acaab79f5ff5
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-17 01:16:07 +0530

    Python.py

---
+++ 01/08.py
@@ -0,0 +1,18 @@
+# Return a String as an Integer
+
+
+print("Create a function that takes a string and returns it as an integer.")
+
+
+def int_str():
+    string = str(input("Enter your Number   :  ")) #string is a String
+    integer_string = int(string)   #Here we converted string into integers
+    print("Here is coverted integers ",integer_string)
+
+
+int_str()
+    
+    
+    # Program  not closed emmidately 
+
+input("Please Click to Enter to End the Program ")
---
+++ 01/09.py
@@ -0,0 +1,27 @@
+# Convert Age to Days
+
+print("Create a function that takes the age in years and returns the age in days.")
+
+def age_days():
+    age = int(input("Enter Your Age : "))
+    years = (age*365)
+    print("Your age in days is - ",str(years))
+
+
+age_days()
+
+# Program  not closed emmidately 
+
+input("Please Click to Enter to End the Program ")
+
+
+
+
+
+
+
+
+
+
+# Program  not closed emmidately 
+input("Please Click to Enter to End the Program ")
