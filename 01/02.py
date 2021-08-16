e1a0243f3c1b3deaf5adbeb6238e80edf7801d50
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-16 17:35:02 +0530

    Python.py

--- 01/02.py
+++ 01/02.py
@@ -3,11 +3,13 @@
 print("Create a function that takes a number as an argument, increments the number by +1 and returns the result.")
 
 def increments():
+    num = int(input("Enter your Number : "))
     i = 0
-    num = int(input("Enter Your Number : "))
     while i<num:
-        print("num")
+        print(i)
         i = i+1
 
 
 increments()
+# terminal not closed emmidately 
+input("Please Click to Enter to End the Program ")
