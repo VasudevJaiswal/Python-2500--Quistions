c115b7a659b8cfa53eb5e806ba0ca3a5840fbc0c
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-16 23:51:58 +0530

    Python.py

---
+++ 01/07.py
@@ -0,0 +1,25 @@
+# There is a single operator in Python, capable of providing the remainder of a division operation. 
+# Two numbers are passed as parameters. 
+# The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+
+
+dividend = int(input("Enter the  Divident Number : "))
+divisor = int(input("Enter the Divisor Number : "))
+
+# Find the division of the operation 
+division = dividend/divisor
+print("Divison of the Operation is - ",str(division))
+
+# Find the quotient of the operation 
+quotient = dividend//divisor
+print("Quotient of the Operation is - ",str(quotient))
+
+
+# Find the remainder of the operation 
+remainder = dividend%divisor
+print("Remainder of the Operation is - ",str(remainder))
+
+
+# Program  not closed emmidately 
+
+input("Please Click to Enter to End the Program ")
