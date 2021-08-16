2ca8a2b65815a721dd6df07e9e7428246a6b3d68
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-17 01:34:54 +0530

    Python.py

---
+++ 01/10.py
@@ -0,0 +1,11 @@
+# Find the Perimeter of a Rectangle
+
+print("Create a function that takes length and width and finds the perimeter of a rectangle.")
+
+def area():
+    H = int(input("Enter Your Height : "))
+    W = int(input("Enter Your Width : "))
+    A = (H*W)
+    print("total area of Rectange - ",A)
+
+area()
