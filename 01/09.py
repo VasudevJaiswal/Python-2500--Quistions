a1d3f765b2918199144ddc852aeb972e09833e18
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-17 01:23:56 +0530

    Python.py

--- 01/09.py
+++ 01/09.py
@@ -1,14 +1,22 @@
 # Convert Age to Days
 
-print("Create a function that takes the age in years and returns the age in days.")
+print("Create a function that takes the age in years,months and returns the age in days.")
 
-def age_days():
+def age_Calender():
     age = int(input("Enter Your Age : "))
+    
+    months = (age*12)
+    print("Your age in Months is - ",str(months))
+    
+     
     years = (age*365)
     print("Your age in days is - ",str(years))
 
 
-age_days()
+
+
+
+age_Calender()
 
 # Program  not closed emmidately 
 
