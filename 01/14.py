cc81996ef0e6c0b0aaa860d0978cc6cbde9276a5
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-17 16:27:59 +0530

    Python.py

---
+++ 01/14.py
@@ -0,0 +1,18 @@
+# Boolean to String Conversion
+
+print("Create a function that takes a boolean variable flag and returns it as a string.")
+
+def bool_to_string():
+    boolean = True
+    string = str(boolean)
+
+
+    print(string)
+    print(type(string))
+
+bool_to_string()
+    
+    
+# Program  not closed emmidately 
+
+input("Please Click to Enter to End the Program ")
---
+++ formula/to_the_power.md
@@ -0,0 +1,9 @@
+# To the power Forumula 
+***
+    use Pow(x,y) function :
+    ** Examples ** 
+                x =   Pow(2,3)
+                print(x)
+            
+            ##Ouput is - 8
+                  
