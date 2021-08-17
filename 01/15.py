6a7ea949452ddd5ac44210105236857c83d5d930
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-17 16:46:00 +0530

    Python.py

---
+++ 01/15.py
@@ -0,0 +1,6 @@
+# Correct the Mistakes
+
+print("Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.")
+
+def squared(b):
+
