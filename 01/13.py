1512ba64980e908a6eb438e840a55dac08f76a59
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-17 15:48:58 +0530

    Python.py

---
+++ 01/13.py
@@ -0,0 +1,7 @@
+# To the Power of _____
+
+print("Create a function that takes a base number and an exponent number and returns the calculation.")
+
+def to_the_power():
+    num = int(input("Enter Your Number : "))
+    
