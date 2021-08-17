b9371f48e5c1ac1fc8bdeb54679cbe71ec7b1567
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-17 16:03:00 +0530

    Python.py

--- 01/13.py
+++ 01/13.py
@@ -3,5 +3,14 @@
 print("Create a function that takes a base number and an exponent number and returns the calculation.")
 
 def to_the_power():
-    num = int(input("Enter Your Number : "))
-    
+    num = int(input("Enter Your Number  : "))
+    power = int(input("Enter Your to the power of Number : "))
+    calculate = pow(num,power)
+    print("To the Power of  - ",str(calculate))
+
+to_the_power()
+
+
+# Program  not closed emmidately 
+
+input("Please Click to Enter to End the Program ")
