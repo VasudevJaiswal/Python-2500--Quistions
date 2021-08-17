5cf16c6fb23dd3a26f3dce6d4757f0eb70bc9af7
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-17 16:54:09 +0530

    Python.py

---
+++ 01/16.py
@@ -0,0 +1,11 @@
+# Return the First Element in a List
+
+print("Create a function that takes a list containing only numbers and return the first element.")
+
+def list_first_value(list):
+    x = y = z = list
+    print(list[0])
+
+
+
+list_first_value(2,3,5)
