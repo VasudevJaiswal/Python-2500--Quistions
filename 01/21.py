a4daa857cbcf4c61e836c510036c26c356dd5eac
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-21 23:31:21 +0530

    Python.py

---
+++ 01/21.py
@@ -0,0 +1,9 @@
+# Burglary Series (14): Adjectives Total
+
+
+print('''You call your spouse in anger and a "little" argument takes place. Count the total amount of adjectives used. Given a dictionary of adjectives, 
+return the total amount of adjectives used.''')
+
+d = {'key1': 'val1', 'key2': 'val2', 'key3': 'val3','key3': 'val3'}
+for d in range:
+    print(d)
