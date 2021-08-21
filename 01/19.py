2c69a44471fa023fd42839e6d3102aa6ee1a2b68
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-21 23:00:35 +0530

    Python.py

---
+++ 01/19.py
@@ -0,0 +1,7 @@
+# Basic Variable Assignment
+
+print('''A student learning Python was trying to make a function.
+ His code should concatenate a passed string name with string  "Vasudev" and 
+ store it in a variable called result. He needs your help to fix this code. ''')
+
+   
